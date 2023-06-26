a7c32ac7ad9aa8cfa2cb9b322f3dbde3e85637e4
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2023-06-26 18:25:41 +0200

    Kotlin code improvements

--- src/main/kotlin/com/stewonello/fertilizer/components/Badge.kt
+++ src/main/kotlin/com/stewonello/fertilizer/components/Badge.kt
@@ -5,14 +5,8 @@ import com.stewonello.fertilizer.dialect.FertilizerComponentContext
 
 class Badge(componentContext: FertilizerComponentContext) : FertilizerComponent(componentContext) {
 
-    var type: String?
+    var type = componentContext.attributes["type"]?.toString() ?: "default"
 
-    init {
-        this.type = componentContext.attributes["type"].toString()
-    }
-
-    public fun classNames(): String {
-        return "badge " + (if (this.type == "danger") "bg-danger" else "bg-secondary")
-    }
-
+    fun classNames() =
+        "badge " + (if (this.type == "danger") "bg-danger" else "bg-secondary")
 }
--- src/main/kotlin/com/stewonello/fertilizer/components/Card.kt
+++ src/main/kotlin/com/stewonello/fertilizer/components/Card.kt
@@ -5,14 +5,7 @@ import com.stewonello.fertilizer.dialect.FertilizerComponentContext
 
 class Card(componentContext: FertilizerComponentContext) : FertilizerComponent(componentContext) {
 
-    val href: String?
-    var headingLevel: Int = 1
-
-    init {
-        href = componentContext.attributes["href"]?.toString()
-        if (componentContext.attributes["heading-level"] is String) {
-            headingLevel = (componentContext.attributes["heading-level"] as String).toInt()
-        }
-    }
+    val href = componentContext.attributes["href"]?.toString()
+    val headingLevel = componentContext.attributes["heading-level"]?.toString()?.toInt()
 
 }
